1b86911c410a469e8f35377713b4d0927e6b9317
kathiravan vellaisamy <kathiravanvellaisamy@gmail.com>
2025-03-14 12:45:01 +0530

    update job

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -13,7 +13,12 @@ export const getAlljobs = async (req, res) => {
 export const getSingleJob = async (req, res) => {
   try {
     const job = await Job.findById(req.params.id);
-    return res.status(200).json(job);
+
+    if (job == null) {
+      return res.status(404).json({ message: "Can not find job" });
+    } else {
+      res.status(200).json(job);
+    }
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
@@ -42,8 +47,30 @@ export const postJob = async (req, res) => {
   }
 };
 
-export const updateJob = (req, res) => {
-  res.json({ message: "Update a new job" });
+export const updateJob = async (req, res) => {
+  try {
+    const updateJob = await Job.findOneAndUpdate(
+      { _id: req.params.id },
+      {
+        title: req.body.title,
+        jobType: req.body.jobType,
+        description: req.body.description,
+        salary: req.body.salary,
+        location: req.body.location,
+        companyName: req.body.companyName,
+        companyDescription: req.body.companyDescription,
+        contactEmail: req.body.contactEmail,
+        contactNumber: req.body.contactNumber,
+      },
+      {
+        new: true,
+      }
+    );
+
+    return res.json(updateJob).status(200);
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
 };
 
 export const deleteJob = () => {
