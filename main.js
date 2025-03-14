7f9a94bf2dff7470a454c27a9aba15b0c9fbf130
kathiravan vellaisamy <kathiravanvellaisamy@gmail.com>
2025-03-14 07:46:28 +0530

    route structure

--- main.js
+++ main.js
@@ -7,6 +7,21 @@ app.get("/", (req, res) => {
   res.json({ message: "Hello Develeopers" });
 });
 
+// Get all the jobs
+app.get("/jobs", (req, res) => {});
+
+// Post a single jobs
+app.post("/jobs", (req, res) => {});
+
+// Get a single Jobs
+app.get("/jobs/:id", (res, req) => {});
+
+// Update a jobs
+app.put("/jobs/:id", (req, res) => {});
+
+// Delete a jobs
+app.delete("/jobs/:id", (req, res) => {});
+
 app.listen(PORT, () => {
   console.log(`The server is running at http://localhost:${PORT}`);
 });
